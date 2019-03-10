adaac4c5863361d1e70f8ab50ac0d3e07901edaf
Rayman <touhourayman@gmail.com>
2019-03-10 19:21:33 +0800

    fix problem of view bound

--- Cesium-3D-Wind/util.js
+++ Cesium-3D-Wind/util.js
@@ -6,26 +6,32 @@ var Util = (function () {
 		return request.responseText;
 	}
 
-	var rectangleToMinMax = function (viewRectangle) {
+	var rectangleToLonLatBound = function (viewRectangle) {
 		var result = {};
+		result.min = {};
+		result.max = {};
 
 		var west = Cesium.Math.toDegrees(viewRectangle.west);
 		var east = Cesium.Math.toDegrees(viewRectangle.east);
+		var width = Cesium.Math.toDegrees(viewRectangle.width);
 
 		var south = Cesium.Math.toDegrees(viewRectangle.south);
 		var north = Cesium.Math.toDegrees(viewRectangle.north);
+		var height = Cesium.Math.toDegrees(viewRectangle.height);
 
-		result.min = {
-			lon: Math.min(east, west), // in Cesium.Rectangle, west may be larger than east
-			lat: south,
-			lev: undefined, // need to be defined later
-		};
+		// in Cesium.Rectangle, west may be larger than east
+		result.min.lon = Cesium.Math.clamp(Math.min(east, west) - width / 2, -180.0, 180.0);
+		result.max.lon = Cesium.Math.clamp(Math.max(east, west) + width / 2, -180.0, 180.0);
 
-		result.max = {
-			lon: Math.max(east, west),
-			lat: north,
-			lev: undefined,
-		};
+		result.min.lat = Cesium.Math.clamp(south - height / 2, -90.0, 90.0);
+		result.max.lat = Cesium.Math.clamp(north + height / 2, -90.0, 90.0);
+
+		if (result.min.lat < -65.0) {
+			result.min.lat = -90.0;
+		}
+		if (result.max.lat > -65.0) {
+			result.max.lat = 90.0;
+		}
 
 		return result;
 	}
@@ -40,7 +46,7 @@ var Util = (function () {
 
 	return {
 		getShaderCode: getShaderCode,
-		rectangleToMinMax: rectangleToMinMax,
+		rectangleToLonLatBound: rectangleToLonLatBound,
 		debug: debug
 	};
 })();
