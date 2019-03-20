30e098720de510367e42c006dc458d4f55badfe1
Rayman <touhourayman@gmail.com>
2019-03-20 16:15:48 +0800

    disable Spector.js on demo page
    Former-commit-id: 260ef6f4ece574150428014d2a359d8909ba490c [formerly 659db840e750b0ad5dbc587fa4ee2fad8c4b4b49]
    Former-commit-id: dae2b20d832368828872c91b48004aed51ec723e
    Former-commit-id: bf83dccf715ceac273b96f7faace4d2e9474549a

--- Cesium-3D-Wind/app.js
+++ Cesium-3D-Wind/app.js
@@ -1,7 +1,7 @@
 // Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmYWEyMmExOS1lYjA2LTQ1YjItOTMwMS03ZWYwMzg1MWY3NWYiLCJpZCI6NDY4OCwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0MTQyMDMzMX0.e5QtAVvpj2oWYIiXyN5oEsFvxF6buKxhj-oOx0L1g7M';
 
 const mode = {
-    debug: true,
+    debug: demo ? false : true,
     offline: true
 };
 var panel = new Panel();
