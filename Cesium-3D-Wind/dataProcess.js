d4ebc018589b825c530c3c6d6dfa424e9aac2081
Rayman <touhourayman@gmail.com>
2019-04-17 10:02:46 +0800

    replace the wrong inversed data
    Former-commit-id: 0c7581153d018f2b8c1e19abd7e54f94d87c4c67 [formerly 4e366540c9c4a41578bb3f4d0a5582db49d4de52]
    Former-commit-id: 5879461ed7124f697f3051a2cbfd936fac822241
    Former-commit-id: 04b757cfeb5c85d262be1869c0e9da77c58f0842

--- Cesium-3D-Wind/dataProcess.js
+++ Cesium-3D-Wind/dataProcess.js
@@ -25,26 +25,23 @@ var DataProcess = (function () {
                 data.dimensions.lev = dimensions['lev'].size;
 
                 var variables = arrayToMap(NetCDF.variables);
-                var lonAttributes = arrayToMap(variables['lon'].attributes);
-                var latAttributes = arrayToMap(variables['lat'].attributes);
-                var levAttributes = arrayToMap(variables['lev'].attributes);
                 var uAttributes = arrayToMap(variables['U'].attributes);
                 var vAttributes = arrayToMap(variables['V'].attributes);
 
                 data.lon = {};
                 data.lon.array = new Float32Array(NetCDF.getDataVariable('lon').flat());
-                data.lon.min = lonAttributes['min'].value;
-                data.lon.max = lonAttributes['max'].value;
+                data.lon.min = Math.min(...data.lon.array);
+                data.lon.max = Math.max(...data.lon.array);
 
                 data.lat = {};
                 data.lat.array = new Float32Array(NetCDF.getDataVariable('lat').flat());
-                data.lat.min = latAttributes['min'].value;
-                data.lat.max = latAttributes['max'].value;
+                data.lat.min = Math.min(...data.lat.array);
+                data.lat.max = Math.max(...data.lat.array);
 
                 data.lev = {};
                 data.lev.array = new Float32Array(NetCDF.getDataVariable('lev').flat());
-                data.lev.min = levAttributes['min'].value;
-                data.lev.max = levAttributes['max'].value;
+                data.lev.min = Math.min(...data.lev.array);
+                data.lev.max = Math.max(...data.lev.array);
 
                 data.U = {};
                 data.U.array = new Float32Array(NetCDF.getDataVariable('U').flat());
